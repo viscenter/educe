96514ef72e936e595393dd3be44a938e8737a46d
Ryan Baumann <ryan.baumann@gmail.com>
2008-02-05 15:53:16 +0000

    Move config data into header, add plugins to compilation skeleton
    git-svn-id: http://rfbaumann.com/educe/trunk/Seg3D@8 06839581-9241-0410-bd91-d75dbdeb4542

---
+++ src/StandAlone/Apps/Seg3D/config.h
@@ -0,0 +1,12 @@
+//    File   : config.h
+//    Author : Ryan Baumann
+//    Date   : Mon Feb  4 16:13:28 EST 2008
+
+#ifndef SEG3D_config_h
+#define SEG3D_config_h
+
+// Where to look for the data directory
+#define DATAPATH "./data"
+//#define DATAPATH "/usr/share/Seg3D/1.9.0/data"
+
+#endif // SEG3D_config_h
