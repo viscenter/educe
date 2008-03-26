72f1dbb25947fb2a69e61d0a69020b0bd112b8fa
Ryan Baumann <ryan.baumann@gmail.com>
2008-03-26 20:33:13 +0000

    Add skeleton for clean recon
    git-svn-id: http://rfbaumann.com/educe/trunk@78 06839581-9241-0410-bd91-d75dbdeb4542

---
+++ recon/recon_clean.c
@@ -0,0 +1,36 @@
+#include <stdio>
+
+#include "mpi.h"
+
+void check_failure(int failure_state);
+
+int main( int argc, char *argv[] )
+{
+	int myid;
+	int failure_state = 0;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+
+	// check for the correct number of command-line arguments
+	if(myid == 0) {
+		if(argc != 7) {
+			printf("Usage: %s slicechunk sinogram number output imagesize cor\n",argv[0]);
+			failure_state = 1;
+		}
+	}
+	check_failure(failure_state);
+
+	return 0;
+}
+
+// broadcast the failure state and exit accordingly
+void check_failure(int failure_state)
+{
+	MPI_Bcast(&failure_state, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	if(failure_state) {
+		MPI_Finalize();
+		exit(1);
+	}
+}
