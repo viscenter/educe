90fa996685c10c61c8c886e513a77998c991c90f
Ryan Baumann <ryan.baumann@gmail.com>
2008-04-15 17:03:06 +0000

    Fix binary chunking util
    git-svn-id: http://rfbaumann.com/educe/trunk@110 06839581-9241-0410-bd91-d75dbdeb4542

--- utils/nrrdchunkbin.cc
+++ utils/nrrdchunkbin.cc
@@ -39,7 +39,7 @@ void save_nrrd_header(char * filename, Nrrd * nout, NrrdIoState * nio)
 	nio->detachedHeader = AIR_TRUE;
 	nio->skipData = AIR_TRUE;
 
-	printf("Skip: %d\n",nio->byteSkip);
+	printf("Data file: %s\n", nio->dataFN[0]);
 	if(!(fileOut = airFopen(filename, stdout, "wb"))) {
 		fprintf(stderr, "Couldn't fopen(\"%s\",\"wb\"): %s\n",
 				filename, strerror(errno));
@@ -57,30 +57,78 @@ void save_nrrd_header(char * filename, Nrrd * nout, NrrdIoState * nio)
 	airMopOkay(mop);
 }
 
-void chunk_nrrd_by_ram_limit(char * filename, Nrrd * nin, NrrdIoState * nio, int ram_limit)
+void chunk_nrrd_by_ram_limit(char * filename, Nrrd * nin, NrrdIoState * nio, long ram_limit)
 {
 	int element_size_in_nrrd = (int)nrrdElementSize(nin);
-	int size_in_ram = (int)nrrdElementNumber(nin)*ELEMENT_SIZE_IN_RAM;
+	long size_in_ram = (long)nrrdElementNumber(nin)*(long)ELEMENT_SIZE_IN_RAM;
 	int chunks = (int)ceilf((float)size_in_ram / (float)ram_limit);
-	int slice_size = nin->axis[0].size * nin->axis[1].size * ELEMENT_SIZE_IN_RAM;
-	char buffer[MAXPATHLEN];
+	long slice_size = nin->axis[0].size * nin->axis[1].size * ELEMENT_SIZE_IN_RAM;
+	char nhdrbuffer[MAXPATHLEN], nrrdbuffer[MAXPATHLEN];
+	char *origDataFN = nio->dataFN[0];
 
+	FILE *nrrdDataIn;
+
+	memset((void*)nrrdbuffer, 0, MAXPATHLEN);
+	snprintf(nrrdbuffer, MAXPATHLEN, "%s%s", nio->path, (nio->dataFN[0])+1);
+	printf("Opening %s as data\n", nrrdbuffer);
+	if((nrrdDataIn = fopen(nrrdbuffer,"r")) == NULL) {
+		perror("Unable to open NRRD data file for reading");
+		exit(1);
+	}
+	
+	// seek the initial BS then reset it
+	if(fseek(nrrdDataIn, nio->byteSkip, SEEK_SET) == -1) {
+		perror("Unable to seek NRRD data file past header");
+		exit(1);
+	}
+	nio->byteSkip = 0;
+	
 	printf("Splitting into %d chunks\n", chunks);
 	for(int i = 0; i < chunks; i++) {
-		memset((void*)buffer, 0, MAXPATHLEN);
-		snprintf(buffer, MAXPATHLEN, "%s-%d.nhdr", filename, i);
+		memset((void*)nhdrbuffer, 0, MAXPATHLEN);
+		memset((void*)nrrdbuffer, 0, MAXPATHLEN);
+		snprintf(nhdrbuffer, MAXPATHLEN, "%s-%d.nhdr", filename, i);
+		snprintf(nrrdbuffer, MAXPATHLEN, "%s-%d.raw", filename, i);
 
-		int bytes_this_chunk = ram_limit > size_in_ram ? size_in_ram : ram_limit;
-		int slices_this_chunk = bytes_this_chunk / slice_size;
+		long bytes_this_chunk = ram_limit > size_in_ram ? size_in_ram : ram_limit;
+		int slices_this_chunk = (int)floor((double)bytes_this_chunk / slice_size);
+		bytes_this_chunk = slices_this_chunk * slice_size;
 
 		nin->axis[2].size = slices_this_chunk;
-
-		save_nrrd_header(buffer, nin, nio);
+		nio->dataFN[0] = nrrdbuffer;
 		
-		nio->byteSkip += (bytes_this_chunk / ELEMENT_SIZE_IN_RAM) *
-			element_size_in_nrrd;
+		save_nrrd_header(nhdrbuffer, nin, nio);
+		
+		// Write out the chunk
+		char *databuffer;
+		long bytes_this_chunk_in_input = bytes_this_chunk; // (bytes_this_chunk / (long)ELEMENT_SIZE_IN_RAM) * (long)element_size_in_nrrd;
+	 	if((databuffer = (char*)malloc(bytes_this_chunk_in_input*sizeof(char))) == NULL) {
+			perror("Couldn't allocate bytes_this_chunk of memory "
+					"(your ram_limit is probably too high for this machine anyway)");
+			exit(1);
+		}
+
+		FILE *nrrdDataOut;
+		if((nrrdDataOut = fopen(nrrdbuffer,"w")) == NULL) {
+			perror("Unable to open NRRD data file for writing");
+			exit(1);
+		}
+
+		printf("Reading and writing %ld bytes\n", bytes_this_chunk_in_input);
+
+		fread(databuffer, sizeof(char), bytes_this_chunk_in_input, nrrdDataIn);
+		fwrite(databuffer, sizeof(char), bytes_this_chunk_in_input, nrrdDataOut);
+
+		fclose(nrrdDataOut);
+		free(databuffer);
+
+		// fseek(nrrdDataIn, (bytes_this_chunk / (long)ELEMENT_SIZE_IN_RAM) *
+		//	(long)element_size_in_nrrd, SEEK_CUR);
 		size_in_ram -= bytes_this_chunk;
 	}
+
+	fclose(nrrdDataIn);
+	nio->dataFN[0] = origDataFN;
 }
 
 int main(int argc, char * argv[])
